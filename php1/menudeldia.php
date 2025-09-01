e46cdf9b4790d5ba7b5567b7877f85c5669aa8c7
Natanaelbeledo <beledonatanael1@gmail.com>
2025-09-01 11:16:02 -0300

    Add files via upload

---
+++ php1/menudeldia.php
@@ -0,0 +1,16 @@
+<?php
+
+$menu = "lunes";
+
+switch ($menu) {
+    case "lunes":
+    echo "papas fritas";
+    break;
+    case "martes":
+    echo "sopa";
+    break;
+    default:
+    echo "arroz";
+
+}
+?>
---
+++ php1/sosmenor.php
@@ -0,0 +1,10 @@
+<?php
+
+$edad = 16;
+
+if ($edad<18) {
+    echo "sos menor boton";
+} else {
+    echo "sos mayor";
+}
+?>
